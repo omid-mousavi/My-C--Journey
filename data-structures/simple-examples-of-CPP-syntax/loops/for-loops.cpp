76d2ff0cdf8d2eaf7bd8f624e5c934239c4ebff3
omid-mousavi <omidmousavi266@gmail.com>
2023-10-18 18:01:06 +0330

    added more explaination about for loop

--- data-structures/simple-examples-of-CPP-syntax/loops/for-loops.cpp
+++ data-structures/simple-examples-of-CPP-syntax/loops/for-loops.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
   int i = 0;
-  for(i = 0; i < 5; i++) {
+  for(i = 0/*is executed (one time) before the execution of the code block.*/; i < 5/*defines the condition for executing the code block.*/; i++/*is executed (every time) after the code block has been executed.*/) {
     cout << "Hello World" << endl; // this for loop runs code for 5 times
   }
 }
