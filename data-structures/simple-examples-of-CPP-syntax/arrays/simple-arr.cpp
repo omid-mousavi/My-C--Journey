0dfd443f054b61d8eed75a2be8caf53a37369f77
omid-mousavi <omidmousavi266@gmail.com>
2023-10-19 14:19:45 +0330

    optimized for-loop example
    I removed x variable and used global i variable instead.

--- data-structures/simple-examples-of-CPP-syntax/arrays/simple-arr.cpp
+++ data-structures/simple-examples-of-CPP-syntax/arrays/simple-arr.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main() {
   double marks[5], sum = 0; // an array with 5 elements
-  int i, x = 0;
+  int i;
   for(i = 0; i < 5; i++) {
     cout << "Enter your mark: ";
     cin >> marks[i];
     sum += marks[i];
-    x++;
   }
-  cout << "Your average is " << sum / x << endl;
+  cout << "Your average is " << sum / i << endl;
   return 0;
 }
